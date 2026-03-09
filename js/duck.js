c8fc20f4a70826cf912476174beba143ac25b9e1
James <james@jamesmcc.co.uk>
2026-03-09 16:45:18 +0000

    more stuff

--- js/duck.js
+++ js/duck.js
@@ -1,3 +1,5 @@
+const MOVEMENT_SPEED = 5
+
 
 class Duck {
   constructor(x, y, h) {
@@ -11,16 +13,16 @@ class Duck {
     circle(this.x, this.y, 50) // TEMP
   }
   moveUp() {
-    this.y--
+    this.y = this.y - MOVEMENT_SPEED
   }
   moveDown() {
-    this.y++
+    this.y = this.y + MOVEMENT_SPEED
   }
   moveLeft() {
-    this.x--
+    this.x = this.x - MOVEMENT_SPEED
   }
   moveRight() {
-    this.x++
+    this.x = this.x + MOVEMENT_SPEED
   }
   attack() {
     console.log('attack')
