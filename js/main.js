562f1300b84f9b286066d519ca73b3b4fdced2d5
Jess Long <146840057+JesssseL@users.noreply.github.com>
2026-03-09 16:24:07 +0000

    Format Code

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ function setup() {
 
 function draw() {
   background('#7CA5B8');
-  noStroke()
+  noStroke();
   ellipseMode(CENTER);
   // SINK
-  fill('#E3F5DF')
+  fill('#E3F5DF');
   rect(0, 0, 500, 50);
   rect(0, 450, 500, 50);
   rect(0, 0, 50, 500);
   rect(450, 0, 50, 500);
   //TAP
-  fill('#787987')
+  fill('#787987');
   rect(235, 0, 30, 100);
   circle(220, 15, 45);
   circle(280, 15, 45);
   //ENTRANCE
-  fill('#6B8AB1')
+  fill('#6B8AB1');
   rect(440, 200, 60, 100);
   rect(0, 200, 60, 100);
   rect(200, 440, 100, 60);
