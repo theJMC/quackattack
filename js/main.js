6af552490a5b6dd5b078760ae5290a7ae7fea497
Jess Long <146840057+JesssseL@users.noreply.github.com>
2026-03-09 16:28:29 +0000

    Add plug hole

--- js/main.js
+++ js/main.js
@@ -4,21 +4,24 @@ function setup() {
 
 function draw() {
   background('#7CA5B8');
-  noStroke();
+  noStroke()
   ellipseMode(CENTER);
   // SINK
-  fill('#E3F5DF');
+  fill('#D2D3DE')
   rect(0, 0, 500, 50);
   rect(0, 450, 500, 50);
   rect(0, 0, 50, 500);
   rect(450, 0, 50, 500);
   //TAP
-  fill('#787987');
+  fill('#B4B5C1')
   rect(235, 0, 30, 100);
-  circle(220, 15, 45);
-  circle(280, 15, 45);
+  circle(210, 15, 45);
+  circle(290, 15, 45);
+  // PLUG HOLE
+  fill('#787987')
+  circle(250, 145, 45);
   //ENTRANCE
-  fill('#6B8AB1');
+  fill('#6B8AB1')
   rect(440, 200, 60, 100);
   rect(0, 200, 60, 100);
   rect(200, 440, 100, 60);
