21b4db68452e6cb6048c4d2b35b2e9944434fa1e
Jess Long <146840057+JesssseL@users.noreply.github.com>
2026-03-09 16:34:30 +0000

    Add duck

---
+++ js/duck.js
@@ -0,0 +1,28 @@
+
+class Duck {
+  constructor(x, y, h) {
+    this.x = x
+    this.y = y
+    this.health = h
+  }
+  draw() {
+    // draw duck
+    fill('#EFC66F') // TEMP
+    circle(this.x, this.y, 50) // TEMP
+  }
+  moveUp() {
+    this.y++
+  }
+  moveDown() {
+    this.y--
+  }
+  moveLeft() {
+    this.x++
+  }
+  moveRight() {
+    this.x--
+  }
+  attack() {
+    console.log('attack')
+  }
+}
